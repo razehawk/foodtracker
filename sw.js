46ad372d594fb7d93571b7cca40688eeaecb545c
razehawk <121561503+razehawk@users.noreply.github.com>
2026-04-03 00:27:27 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'foodtracker-v1';
+const URLS_TO_CACHE = [
+  './Foodtracker_3.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
